0256d31543b155d30d82addd3eedc1776fd5afd7
matthewsey <matthewsey@hotmail.com>
2017-04-03 09:09:17 +0200

    Copied project into GitHub

---
+++ Go Home/Assets/Script/PlayerMovment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour {
+
+	Rigidbody rigidbody;
+	public float moveSpeed;
+	private Vector3 input;
+
+	// Use this for initialization
+	void Awake () {
+
+		rigidbody = GetComponent <Rigidbody> ();
+		
+	}
+	
+	void Update () {
+
+		input = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		rigidbody.AddForce(input * moveSpeed);
+		
+	}
+}
