7b871f9072896b781051946e4a437dbd0f274608
matthewsey <matthewsey@hotmail.com>
2017-04-30 16:02:13 +0200

    Main Menu
    Created the Main Menu of the game. Start button with animation in the
    background

---
+++ Go Home/Assets/Script/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// This will return the player to the main scene
+	public void NewGame () {
+
+		SceneManager.LoadScene ("Cube");
+
+	}
+
+}
